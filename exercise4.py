84ca272d76846f5e1d1cdb6667e41232930bde69
raul <cuestaliriaraul@gmail.com>
2025-03-09 11:34:56 -0400

    Exercise4

---
+++ exercise4.py
@@ -0,0 +1,21 @@
+
+width = 17
+
+height = 12.0
+
+integer_division = width//2
+
+division_first = width/2.0
+
+division_second = height/3
+
+operation_result = 1+2*5
+
+print(f"The result of integer division is: {integer_division}" + " and it is the type " + str(type(integer_division)))
+
+print(f"The result of first division is: {division_first}" + " and it is the type " + str(type(division_first)))
+
+print(f"The result of second division is: {division_second}" + " and it is the type " + str(type(division_second)))
+
+print(f"The result of operation is: {operation_result}" + " and it is the type " + str(type(operation_result)))
+
