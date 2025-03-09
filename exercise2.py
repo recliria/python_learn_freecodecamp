d688e5c8227387cde3ad019b0e99e21908f4de08
raul <cuestaliriaraul@gmail.com>
2025-03-09 11:03:47 -0400

    Exercise2 and Exercise3

---
+++ exercise2.py
@@ -0,0 +1,4 @@
+
+name = input("Write your name, please: ")
+
+print("Hello " + name)
---
+++ exercise3.py
@@ -0,0 +1,8 @@
+
+hours = int(input("Enter the hours: "))
+
+rate = int(input("Enter the rate: "))
+
+pay = hours * rate
+
+print(f"The pay is: {pay}")
