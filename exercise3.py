6a5ce460a580e82027de18bbde2b57a65fdf30df
raul <cuestaliriaraul@gmail.com>
2025-03-11 15:22:28 -0400

    Rewrite exercise3. It was if conditional

--- exercise3.py
+++ exercise3.py
@@ -3,6 +3,12 @@ hours = int(input("Enter the hours: "))
 
 rate = int(input("Enter the rate: "))
 
-pay = hours * rate
+if hours > 40:
+
+    pay = hours * (rate * 1.5)
+
+else:
+
+    pay = hours * rate
 
 print(f"The pay is: {pay}")
