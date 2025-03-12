3816736edefeb6fcc7ef01fb71c8c3887d008ac7
raul <cuestaliriaraul@gmail.com>
2025-03-12 17:00:18 -0400

    Exercise3 was added compute_pay function

--- exercise3.py
+++ exercise3.py
@@ -5,6 +5,17 @@ try:
 
     rate = int(input("Enter the rate: "))
 
+except:
+
+    print("Error, please enter numeric input")
+
+    hours = int(input("Enter the hours: "))
+
+    rate = int(input("Enter the rate: "))
+
+def compute_pay(hours,rate):    
+    
+
     if hours > 40:
 
         pay = hours * (rate * 1.5)
@@ -13,8 +24,7 @@ try:
 
         pay = hours * rate
 
-    print(f"The pay is: {pay}")
+    print(f"The pay is: {pay}")  
 
-except:
 
-    print("Error, please enter numeric input")
+compute_pay(hours,rate)
